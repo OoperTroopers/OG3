5d69ef31b1d35a99d00286ff1de14d8498549fef
dannysepler <dsepler@ufl.edu>
2015-04-16 10:44:56 -0400

    got rid of a merge conflict i missed!

--- src/model/map/GrassTerrain.java
+++ src/model/map/GrassTerrain.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package model.map;
-<<<<<<< HEAD
 
 import view.modelview.tileable.terrain.GrassTerrainView;
 import view.modelview.tileable.terrain.TerrainView;

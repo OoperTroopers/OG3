14503d939aa30c1227411dad0e2908fdaf457421
Adam Cronin <abcronin@ufl.edu>
2015-04-17 17:03:52 -0400

    Adding controller stuff

---
+++ src/controller/ControllerAvatar.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Adam on 4/17/2015.
+ */
+public class ControllerAvatar {
+}
---
+++ src/model/abilities/Ability.java
@@ -0,0 +1,7 @@
+package model.abilities;
+
+/**
+ * Created by Adam on 4/17/2015.
+ */
+public class Ability {
+}

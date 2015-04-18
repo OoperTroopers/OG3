eec5742c20cd39e34c9b2ac51be60c14e41e0f3b
dannysepler <dsepler@ufl.edu>
2015-04-17 20:37:02 -0400

    cormac has a pulitzer

---
+++ src/view/modelview/areaeffect/LevelUpEffectView.java
@@ -0,0 +1,15 @@
+package view.modelview.areaeffect;
+
+import java.awt.image.BufferedImage;
+
+public class LevelUpEffectView extends AreaEffectView {
+
+	BufferedImage image;
+	
+	@Override
+	public BufferedImage getImage() {
+		// TODO Auto-generated method stub
+		return image;
+	}
+
+}

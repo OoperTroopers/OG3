00c197327d5e340f5140896dc007046d68400812
Cormac McCarthy <cormacpayne@ufl.edu>
2015-04-18 15:23:17 -0400

    okay.....

--- src/view/modelview/tileableview/TerrainView.java
+++
@@ -1,9 +0,0 @@
-package view.modelview.tileableview;
-
-import view.modelview.tileable.TileableView;
-
-
-public abstract class TerrainView implements TileableView {
-    @Override
-    public int getPriority() { return 1; }
-}

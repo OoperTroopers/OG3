bc6517585f0328916844016cb1bca7758db8bbe3
reidholsen <reidh.olsen@ufl.edu>
2015-04-18 18:31:42 -0400

    AreaAffectViews fixed

--- src/view/modelview/areaeffect/InstantDeathEffectView.java
+++ src/view/modelview/areaeffect/InstantDeathEffectView.java
@@ -1,10 +1,21 @@
 package view.modelview.areaeffect;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import view.tools.ImagePaths;
 
 public class InstantDeathEffectView extends AreaEffectView {
 
 	BufferedImage image;
+	
+	public InstantDeathEffectView() {
+		try { image = ImageIO.read(new File(ImagePaths.SKULL));} 
+		catch (IOException e) {}	
+	}
 	
 	@Override
 	public BufferedImage getImage() {

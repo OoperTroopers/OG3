95d03e1869ca23e9d8d1338c22dcf24d603b5063
reidholsen <reidh.olsen@ufl.edu>
2015-04-18 23:52:11 -0400

    NPCBrain

---
+++ src/controller/NPCBrain.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.entities.NPC;
+
+public class NPCBrain implements Runnable{
+	private NPC npc;
+	
+	public NPCBrain(){
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
